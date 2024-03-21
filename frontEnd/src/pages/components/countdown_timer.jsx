8d6b0363a249d1df2620ad039bb65a9fad223ac7
Kian D.J. O'Connor <kiano.admin@serha.gov.jm>
2024-03-21 09:44:22 -0500

    zero timer

--- frontEnd/src/pages/components/countdown_timer.jsx
+++ frontEnd/src/pages/components/countdown_timer.jsx
@@ -1,46 +1,46 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 const CountdownTimer = () => {
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [days, setDays] = useState(0);
+  // const [seconds, setSeconds] = useState(0);
+  // const [minutes, setMinutes] = useState(0);
+  // const [hours, setHours] = useState(0);
+  // const [days, setDays] = useState(0);
 
-  useEffect(() => {
-    const eventDate = new Date("March 20, 2024 00:00:00");
-    const currentDate = new Date().getTime();
+  // useEffect(() => {
+  //   const eventDate = new Date("March 20, 2024 00:00:00");
+  //   const currentDate = new Date().getTime();
 
-    const distance = eventDate - currentDate;
+  //   const distance = eventDate - currentDate;
 
-    setDays(Math.floor(distance / (1000 * 60 * 60 * 24)).toString());
-    setHours(
-      Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      ).toString()
-    );
-    setMinutes(
-      Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString()
-    );
-    setSeconds(Math.floor((distance % (1000 * 60)) / 1000).toString());
+  //   setDays(Math.floor(distance / (1000 * 60 * 60 * 24)).toString());
+  //   setHours(
+  //     Math.floor(
+  //       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //     ).toString()
+  //   );
+  //   setMinutes(
+  //     Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString()
+  //   );
+  //   setSeconds(Math.floor((distance % (1000 * 60)) / 1000).toString());
 
-    setInterval(() => {
-      const currentDate = new Date().getTime();
+  //   setInterval(() => {
+  //     const currentDate = new Date().getTime();
 
-      const distance = eventDate - currentDate;
+  //     const distance = eventDate - currentDate;
 
-      setDays(Math.floor(distance / (1000 * 60 * 60 * 24)).toString());
-      setHours(
-        Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        ).toString()
-      );
-      setMinutes(
-        Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString()
-      );
-      setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
-    }, 1000);
-    // alert(currentDate);
-  }, []);
+  //     setDays(Math.floor(distance / (1000 * 60 * 60 * 24)).toString());
+  //     setHours(
+  //       Math.floor(
+  //         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //       ).toString()
+  //     );
+  //     setMinutes(
+  //       Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString()
+  //     );
+  //     setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
+  //   }, 1000);
+  //   // alert(currentDate);
+  // }, []);
   return (
     <div className="counter-container">
       <div className="counter-header">Time to retreat</div>
@@ -48,12 +48,14 @@ const CountdownTimer = () => {
         <div className="counter-section">
           <div className="counter-numbers">
             <div className="counter-numbers-left" id="days-left">
-            {days.toString().charAt(1) ? days.toString().charAt(0) : 0}
+              {/* {days.toString().charAt(1) ? days.toString().charAt(0) : 0} */}
+              0
             </div>
             <div className="counter-numbers-right" id="days-right">
-            {days.toString().charAt(1)
+              0
+              {/* {days.toString().charAt(1)
                 ? days.toString().charAt(1)
-                : days.toString().charAt(0)}
+                : days.toString().charAt(0)} */}
             </div>
           </div>
           <div className="counter-name">Days</div>
@@ -62,12 +64,14 @@ const CountdownTimer = () => {
         <div className="counter-section">
           <div className="counter-numbers">
             <div className="counter-numbers-left" id="hours-left">
-              {hours.toString().charAt(1) ? hours.toString().charAt(0) : 0}
+              {/* {hours.toString().charAt(1) ? hours.toString().charAt(0) : 0} */}
+              0
             </div>
             <div className="counter-numbers-right" id="hours-right">
-              {hours.toString().charAt(1)
+              {/* {hours.toString().charAt(1)
                 ? hours.toString().charAt(1)
-                : hours.toString().charAt(0)}
+                : hours.toString().charAt(0)} */}
+              0
             </div>
           </div>
           <div className="counter-name">Hours</div>
@@ -76,12 +80,14 @@ const CountdownTimer = () => {
         <div className="counter-section">
           <div className="counter-numbers">
             <div className="counter-numbers-left" id="minutes-left">
-              {minutes.toString().charAt(1) ? minutes.toString().charAt(0) : 0}
+              {/* {minutes.toString().charAt(1) ? minutes.toString().charAt(0) : 0} */}
+              0
             </div>
             <div className="counter-numbers-right" id="minutes-right">
-              {minutes.toString().charAt(1)
+              {/* {minutes.toString().charAt(1)
                 ? minutes.toString().charAt(1)
-                : minutes.toString().charAt(0)}
+                : minutes.toString().charAt(0)} */}
+              0
             </div>
           </div>
           <div className="counter-name">Mins</div>
@@ -90,12 +96,14 @@ const CountdownTimer = () => {
         <div className="counter-section">
           <div className="counter-numbers">
             <div className="counter-numbers-left" id="seconds-left">
-              {seconds.toString().charAt(1) ? seconds.toString().charAt(0) : 0}
+              {/* {seconds.toString().charAt(1) ? seconds.toString().charAt(0) : 0} */}
+              0
             </div>
             <div className="counter-numbers-right" id="seconds-right">
-              {seconds.toString().charAt(1)
+              {/* {seconds.toString().charAt(1)
                 ? seconds.toString().charAt(1)
-                : seconds.toString().charAt(0)}
+                : seconds.toString().charAt(0)} */}
+              0
             </div>
           </div>
           <div className="counter-name">Secs</div>
